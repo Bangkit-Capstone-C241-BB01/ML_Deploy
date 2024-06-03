fcf4c3ebd9289fdd2eef9fd4f23cfb0d00e28255
Carissa Chandra <94051808+Ca-ri-ssa@users.noreply.github.com>
2024-06-03 20:05:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -158,4 +158,5 @@ if not os.path.exists(upload_folder):
     os.makedirs(upload_folder)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
